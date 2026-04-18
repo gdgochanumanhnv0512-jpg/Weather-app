5428ae6bc340ac95ed7a78cf56adb74177717455
gdgochanumanhnv0512-jpg <gdgochanu.manhnv0512@gmail.com>
2026-04-19 06:52:48 +0700

    fixed code

--- browser.js
+++ browser.js
@@ -1,13 +1,20 @@
 const apiKey = "7c973ced62e42a7846ac9cfcc6894628";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=HaNoi&appid=7c973ced62e42a7846ac9cfcc6894628&units=metric";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=hanoi&units=metric"
+const searchBox = document.querySelector("#cityInput");
+const searchBtn = document.querySelector("#button");
 async function checkWeather(city) {
-  const response = await fetch(apiUrl +city+ "&appid=${apiKey}");
+  const response = await fetch(apiUrl +city+ '&appid=${apiKey}');
   var data = await response.json();
   console.log(data);
-  document.querySelector("#temperature").innerText = Math.round(data.main.temp + "℃");
-  document.querySelector("#city").innerText = data.name;
-  document.querySelector("#humidity").innerText = data.main.humidity + "%";
-  document.querySelector("#windspeed").innerText = data.wind.speed;
-
+  document.querySelector("#temperature").innerHTML = Math.round(data.main.temp) + " °k";
+  document.querySelector("#city").innerHTML = data.name;
+  document.querySelector("#windspeed").innerHTML = data.wind.speed + "km/h";
+  document.querySelector("#humidity").innerHTML = data.main.humidity + "%";
 }
-checkWeather();
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+}
+)
+
+
+
